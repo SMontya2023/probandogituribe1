26aa76b72973dfbd71d49f914495267e97589867
felipe9321@hotmail.com <felipe9321@hotmail.com>
2023-11-28 11:30:07 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Natillera Tio Rico
+        //¿Que necesito para procesar la natillera de 1 cliente?
+        //Entradas
+        int idCliente=286578;
+        long documentoIdentidad=114643508400L;
+        long telefonoCliente=3029394850L;
+        byte  edadCliente=32;
+        short   coutaMensualCliente=25000;
+        double tasaInteresFijo=0.1;
+        char   calificacionCliente='A';
+    }
+}
